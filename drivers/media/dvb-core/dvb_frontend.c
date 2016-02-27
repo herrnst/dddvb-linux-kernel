bb475dc2c63352d4a10ac3ca534c1c0554e2a925
Daniel Scheller <d.scheller@gmx.net>
2016-02-27 16:15:42 +0100

    [media] dvb_frontend: add DTV_INPUT/set_input dtv command

--- drivers/media/dvb-core/dvb_frontend.c
+++ drivers/media/dvb-core/dvb_frontend.c
@@ -1047,6 +1047,7 @@ static struct dtv_cmds_h dtv_cmds[DTV_MAX_COMMAND + 1] = {
 	_DTV_CMD(DTV_STREAM_ID, 1, 0),
 	_DTV_CMD(DTV_DVBT2_PLP_ID_LEGACY, 1, 0),
 	_DTV_CMD(DTV_LNA, 1, 0),
+	_DTV_CMD(DTV_INPUT, 1, 0),
 	_DTV_CMD(DTV_PLS, 1, 0),
 
 	/* Get */
@@ -1476,6 +1477,14 @@ static int dtv_property_process_get(struct dvb_frontend *fe,
 
 	case DTV_LNA:
 		tvp->u.data = c->lna;
+		break;
+
+	case DTV_INPUT:
+		tvp->u.buffer.data[0] = c->input;
+		tvp->u.buffer.data[1] = fe->ops.xbar[0];
+		tvp->u.buffer.data[2] = fe->ops.xbar[1];
+		tvp->u.buffer.data[3] = fe->ops.xbar[2];
+		tvp->u.buffer.len = 4;
 		break;
 
 	case DTV_PLS:
@@ -1911,6 +1920,12 @@ static int dtv_property_process_set(struct dvb_frontend *fe,
 			r = fe->ops.set_lna(fe);
 		if (r < 0)
 			c->lna = LNA_AUTO;
+		break;
+
+	case DTV_INPUT:
+		c->input = tvp->u.data;
+		if (fe->ops.set_input)
+			r = fe->ops.set_input(fe, c->input);
 		break;
 
 	case DTV_PLS:
