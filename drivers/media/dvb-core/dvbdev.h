5da6847a34f30e87d2539b67c4a5471415ac1643
Daniel Scheller <d.scheller@gmx.net>
2016-03-06 22:50:50 +0100

    [media] dvb-core: add DVB_DEVICE_CI, ci device name

--- drivers/media/dvb-core/dvbdev.h
+++ drivers/media/dvb-core/dvbdev.h
@@ -48,6 +48,7 @@
 #define DVB_DEVICE_CA         6
 #define DVB_DEVICE_NET        7
 #define DVB_DEVICE_OSD        8
+#define DVB_DEVICE_CI         9
 
 #define DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr) \
 	static short adapter_nr[] = \
