2ab7e945d35f2d84b5787dec44a119fac8ca8cc8
Daniel Scheller <d.scheller@gmx.net>
2017-01-16 20:56:38 +0100

    [media] dvb-frontend: add DTV_PLS dtv command (physical layer scrambling API)
    ref https://github.com/DigitalDevices/dddvb/commit/8908df8098107818d3f79640f55cf9e0431ce008
    ref https://github.com/DigitalDevices/dddvb/commit/2c8fcc1df59d32ebc4a3b7380a3bfdc5c6d332c1

--- drivers/media/dvb-core/dvb_frontend.c
+++ drivers/media/dvb-core/dvb_frontend.c
@@ -984,6 +984,7 @@ static int dvb_frontend_clear_cache(struct dvb_frontend *fe)
 	}
 
 	c->stream_id = NO_STREAM_ID_FILTER;
+	c->pls = NO_SCRAMBLING_CODE;
 
 	switch (c->delivery_system) {
 	case SYS_DVBS:
@@ -1075,6 +1076,7 @@ static struct dtv_cmds_h dtv_cmds[DTV_MAX_COMMAND + 1] = {
 	_DTV_CMD(DTV_STREAM_ID, 1, 0),
 	_DTV_CMD(DTV_DVBT2_PLP_ID_LEGACY, 1, 0),
 	_DTV_CMD(DTV_LNA, 1, 0),
+	_DTV_CMD(DTV_PLS, 1, 0),
 
 	/* Get */
 	_DTV_CMD(DTV_DISEQC_SLAVE_REPLY, 0, 1),
@@ -1503,6 +1505,10 @@ static int dtv_property_process_get(struct dvb_frontend *fe,
 
 	case DTV_LNA:
 		tvp->u.data = c->lna;
+		break;
+
+	case DTV_PLS:
+		tvp->u.data = c->pls;
 		break;
 
 	/* Fill quality measures */
@@ -1934,6 +1940,10 @@ static int dtv_property_process_set(struct dvb_frontend *fe,
 			r = fe->ops.set_lna(fe);
 		if (r < 0)
 			c->lna = LNA_AUTO;
+		break;
+
+	case DTV_PLS:
+		c->pls = tvp->u.data;
 		break;
 
 	default:
