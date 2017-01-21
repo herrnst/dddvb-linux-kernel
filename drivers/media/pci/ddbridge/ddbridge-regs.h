6cba7e5aff30c89705a4befca5dfc25765ee50a7
Daniel Scheller <d.scheller@gmx.net>
2017-01-21 18:56:41 +0100

    [media] ddbridge: clean up ddbridge-regs

--- drivers/media/pci/ddbridge/ddbridge-regs.h
+++ drivers/media/pci/ddbridge/ddbridge-regs.h
@@ -17,15 +17,6 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-/* Register Definitions */
-
-#define CUR_REGISTERMAP_VERSION     0x10003
-#define CUR_REGISTERMAP_VERSION_CI  0x10000
-#define CUR_REGISTERMAP_VERSION_MOD 0x10000
-
-#define HARDWARE_VERSION       0x00
-#define REGISTERMAP_VERSION    0x04
-
 /* ------------------------------------------------------------------------- */
 /* SPI Controller */
 
@@ -61,7 +52,6 @@
 #define INTERRUPT_BASE   (0x40)
 
 #define INTERRUPT_ENABLE (INTERRUPT_BASE + 0x00)
-#define MSI0_ENABLE      (INTERRUPT_BASE + 0x00)
 #define MSI1_ENABLE      (INTERRUPT_BASE + 0x04)
 #define MSI2_ENABLE      (INTERRUPT_BASE + 0x08)
 #define MSI3_ENABLE      (INTERRUPT_BASE + 0x0C)
@@ -72,51 +62,6 @@
 
 #define INTERRUPT_STATUS (INTERRUPT_BASE + 0x20)
 #define INTERRUPT_ACK    (INTERRUPT_BASE + 0x20)
-
-#define INTMASK_CLOCKGEN    (0x00000001)
-#define INTMASK_TEMPMON     (0x00000002)
-
-#define INTMASK_I2C1        (0x00000001)
-#define INTMASK_I2C2        (0x00000002)
-#define INTMASK_I2C3        (0x00000004)
-#define INTMASK_I2C4        (0x00000008)
-
-#define INTMASK_CIRQ1       (0x00000010)
-#define INTMASK_CIRQ2       (0x00000020)
-#define INTMASK_CIRQ3       (0x00000040)
-#define INTMASK_CIRQ4       (0x00000080)
-
-#define INTMASK_TSINPUT1    (0x00000100)
-#define INTMASK_TSINPUT2    (0x00000200)
-#define INTMASK_TSINPUT3    (0x00000400)
-#define INTMASK_TSINPUT4    (0x00000800)
-#define INTMASK_TSINPUT5    (0x00001000)
-#define INTMASK_TSINPUT6    (0x00002000)
-#define INTMASK_TSINPUT7    (0x00004000)
-#define INTMASK_TSINPUT8    (0x00008000)
-
-#define INTMASK_TSOUTPUT1   (0x00010000)
-#define INTMASK_TSOUTPUT2   (0x00020000)
-#define INTMASK_TSOUTPUT3   (0x00040000)
-#define INTMASK_TSOUTPUT4   (0x00080000)
-
-#define INTERRUPT_V2_CONTROL  (INTERRUPT_BASE + 0x00)
-#define INTERRUPT_V2_ENABLE_1 (INTERRUPT_BASE + 0x04)
-#define INTERRUPT_V2_ENABLE_2 (INTERRUPT_BASE + 0x08)
-#define INTERRUPT_V2_ENABLE_3 (INTERRUPT_BASE + 0x0c)
-#define INTERRUPT_V2_ENABLE_4 (INTERRUPT_BASE + 0x10)
-#define INTERRUPT_V2_ENABLE_5 (INTERRUPT_BASE + 0x14)
-#define INTERRUPT_V2_ENABLE_6 (INTERRUPT_BASE + 0x18)
-#define INTERRUPT_V2_ENABLE_7 (INTERRUPT_BASE + 0x1c)
-
-#define INTERRUPT_V2_STATUS   (INTERRUPT_BASE + 0x20)
-#define INTERRUPT_V2_STATUS_1 (INTERRUPT_BASE + 0x24)
-#define INTERRUPT_V2_STATUS_2 (INTERRUPT_BASE + 0x28)
-#define INTERRUPT_V2_STATUS_3 (INTERRUPT_BASE + 0x2c)
-#define INTERRUPT_V2_STATUS_4 (INTERRUPT_BASE + 0x30)
-#define INTERRUPT_V2_STATUS_5 (INTERRUPT_BASE + 0x34)
-#define INTERRUPT_V2_STATUS_6 (INTERRUPT_BASE + 0x38)
-#define INTERRUPT_V2_STATUS_7 (INTERRUPT_BASE + 0x3c)
 
 /* Temperature Monitor ( 2x LM75A @ 0x90,0x92 I2c ) */
 #define TEMPMON_BASE			(0x1c0)
@@ -141,33 +86,14 @@
 #define I2C_TASKADDRESS (0x0C)     /* High read, low write */
 #define I2C_MONITOR     (0x1C)
 
-#define I2C_SPEED_666   (0x02010202)
 #define I2C_SPEED_400   (0x04030404)
-#define I2C_SPEED_200   (0x09080909)
-#define I2C_SPEED_154   (0x0C0B0C0C)
 #define I2C_SPEED_100   (0x13121313)
-#define I2C_SPEED_77    (0x19181919)
-#define I2C_SPEED_50    (0x27262727)
-
 
 /* ------------------------------------------------------------------------- */
 /* DMA  Controller */
 
 #define DMA_BASE_WRITE        (0x100)
 #define DMA_BASE_READ         (0x140)
-
-#define DMA_CONTROL     (0x00)
-#define DMA_ERROR       (0x04)
-
-#define DMA_DIAG_CONTROL                (0x1C)
-#define DMA_DIAG_PACKETCOUNTER_LOW      (0x20)
-#define DMA_DIAG_PACKETCOUNTER_HIGH     (0x24)
-#define DMA_DIAG_TIMECOUNTER_LOW        (0x28)
-#define DMA_DIAG_TIMECOUNTER_HIGH       (0x2C)
-#define DMA_DIAG_RECHECKCOUNTER         (0x30)
-#define DMA_DIAG_WAITTIMEOUTINIT        (0x34)
-#define DMA_DIAG_WAITOVERFLOWCOUNTER    (0x38)
-#define DMA_DIAG_WAITCOUNTER            (0x3C)
 
 #define TS_CONTROL(_io)         (_io->regs + 0x00)
 #define TS_CONTROL2(_io)        (_io->regs + 0x04)
@@ -217,112 +143,16 @@
 #define CI_RESET_CAM                    (0x00000001)
 #define CI_POWER_ON                     (0x00000002)
 #define CI_ENABLE                       (0x00000004)
-#define CI_BLOCKIO_ENABLE               (0x00000008)
 #define CI_BYPASS_DISABLE               (0x00000010)
-#define CI_DISABLE_AUTO_OFF             (0x00000020)
 
 #define CI_CAM_READY                    (0x00010000)
 #define CI_CAM_DETECT                   (0x00020000)
 #define CI_READY                        (0x80000000)
-#define CI_BLOCKIO_ACTIVE               (0x40000000)
-#define CI_BLOCKIO_RCVDATA              (0x20000000)
-#define CI_BLOCKIO_SEND_PENDING         (0x10000000)
-#define CI_BLOCKIO_SEND_COMPLETE        (0x08000000)
 
 #define CI_READ_CMD                     (0x40000000)
 #define CI_WRITE_CMD                    (0x80000000)
 
-#define CI_BLOCKIO_SEND(i)              (CI_BASE + (i) * 32 + 0x14)
-#define CI_BLOCKIO_RECEIVE(i)           (CI_BASE + (i) * 32 + 0x18)
-
-#define CI_BLOCKIO_SEND_COMMAND         (0x80000000)
-#define CI_BLOCKIO_SEND_COMPLETE_ACK    (0x40000000)
-#define CI_BLOCKIO_RCVDATA_ACK          (0x40000000)
-
 #define CI_BUFFER_BASE                  (0x3000)
 #define CI_BUFFER_SIZE                  (0x0800)
-#define CI_BLOCKIO_BUFFER_SIZE          (CI_BUFFER_SIZE/2)
 
 #define CI_BUFFER(i)                  (CI_BUFFER_BASE + (i) * CI_BUFFER_SIZE)
-#define CI_BLOCKIO_RECEIVE_BUFFER(i)  (CI_BUFFER_BASE + (i) * CI_BUFFER_SIZE)
-#define CI_BLOCKIO_SEND_BUFFER(i)  \
-	(CI_BUFFER_BASE + (i) * CI_BUFFER_SIZE + CI_BLOCKIO_BUFFER_SIZE)
-
-#define VCO1_BASE           (0xC0)
-#define VCO1_CONTROL        (VCO1_BASE + 0x00)
-#define VCO1_DATA           (VCO1_BASE + 0x04)  /* 24 Bit */
-/* 1 = Trigger write, resets when done */
-#define VCO1_CONTROL_WRITE  (0x00000001)
-/* 0 = Put VCO into power down */
-#define VCO1_CONTROL_CE     (0x00000002)
-/* Muxout from VCO (usually = Lock) */
-#define VCO1_CONTROL_MUXOUT (0x00000004)
-
-#define VCO2_BASE           (0xC8)
-#define VCO2_CONTROL        (VCO2_BASE + 0x00)
-#define VCO2_DATA           (VCO2_BASE + 0x04)  /* 24 Bit */
-/* 1 = Trigger write, resets when done */
-#define VCO2_CONTROL_WRITE  (0x00000001)
-/* 0 = Put VCO into power down */
-#define VCO2_CONTROL_CE     (0x00000002)
-/* Muxout from VCO (usually = Lock) */
-#define VCO2_CONTROL_MUXOUT (0x00000004)
-
-#define VCO3_BASE           (0xD0)
-#define VCO3_CONTROL        (VCO3_BASE + 0x00)
-#define VCO3_DATA           (VCO3_BASE + 0x04)  /* 32 Bit */
-/* 1 = Trigger write, resets when done */
-#define VCO3_CONTROL_WRITE  (0x00000001)
-/* 0 = Put VCO into power down */
-#define VCO3_CONTROL_CE     (0x00000002)
-/* Muxout from VCO (usually = Lock) */
-#define VCO3_CONTROL_MUXOUT (0x00000004)
-
-#define RF_ATTENUATOR   (0xD8)
-/*  0x00 =  0 dB
- *  0x01 =  1 dB
- *    ...
- *  0x1F = 31 dB
- */
-
-#define RF_POWER        (0xE0)
-#define RF_POWER_BASE       (0xE0)
-#define RF_POWER_CONTROL    (RF_POWER_BASE + 0x00)
-#define RF_POWER_DATA       (RF_POWER_BASE + 0x04)
-
-#define RF_POWER_CONTROL_START     (0x00000001)
-#define RF_POWER_CONTROL_DONE      (0x00000002)
-#define RF_POWER_CONTROL_VALIDMASK (0x00000700)
-#define RF_POWER_CONTROL_VALID     (0x00000500)
-
-
-/* --------------------------------------------------------------------------
- * Output control
- */
-
-#define IQOUTPUT_BASE           (0x240)
-#define IQOUTPUT_CONTROL        (IQOUTPUT_BASE + 0x00)
-#define IQOUTPUT_CONTROL2       (IQOUTPUT_BASE + 0x04)
-#define IQOUTPUT_PEAK_DETECTOR  (IQOUTPUT_BASE + 0x08)
-#define IQOUTPUT_POSTSCALER     (IQOUTPUT_BASE + 0x0C)
-#define IQOUTPUT_PRESCALER      (IQOUTPUT_BASE + 0x10)
-
-#define IQOUTPUT_EQUALIZER_0    (IQOUTPUT_BASE + 0x14)
-#define IQOUTPUT_EQUALIZER_1    (IQOUTPUT_BASE + 0x18)
-#define IQOUTPUT_EQUALIZER_2    (IQOUTPUT_BASE + 0x1C)
-#define IQOUTPUT_EQUALIZER_3    (IQOUTPUT_BASE + 0x20)
-#define IQOUTPUT_EQUALIZER_4    (IQOUTPUT_BASE + 0x24)
-#define IQOUTPUT_EQUALIZER_5    (IQOUTPUT_BASE + 0x28)
-#define IQOUTPUT_EQUALIZER_6    (IQOUTPUT_BASE + 0x2C)
-#define IQOUTPUT_EQUALIZER_7    (IQOUTPUT_BASE + 0x30)
-#define IQOUTPUT_EQUALIZER_8    (IQOUTPUT_BASE + 0x34)
-#define IQOUTPUT_EQUALIZER_9    (IQOUTPUT_BASE + 0x38)
-#define IQOUTPUT_EQUALIZER_10   (IQOUTPUT_BASE + 0x3C)
-
-#define IQOUTPUT_EQUALIZER(i)   (IQOUTPUT_EQUALIZER_0 + (i) * 4)
-
-#define IQOUTPUT_CONTROL_RESET              (0x00000001)
-#define IQOUTPUT_CONTROL_ENABLE             (0x00000002)
-#define IQOUTPUT_CONTROL_RESET_PEAK         (0x00000004)
-#define IQOUTPUT_CONTROL_ENABLE_PEAK        (0x00000008)
-#define IQOUTPUT_CONTROL_BYPASS_EQUALIZER   (0x00000010)
