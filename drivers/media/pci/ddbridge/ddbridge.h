73bd80987f27bdfbc6b6406002f77a90f15d3b70
Daniel Scheller <d.scheller@gmx.net>
2017-01-25 19:36:12 +0100

    [media] ddbridge: strip out GTL related code which only seems to be used on OctoNet devices

--- drivers/media/pci/ddbridge/ddbridge.h
+++ drivers/media/pci/ddbridge/ddbridge.h
@@ -95,7 +95,6 @@ struct ddb_regmap {
 	u32 irq_base_i2c;
 	u32 irq_base_idma;
 	u32 irq_base_odma;
-	u32 irq_base_gtl;
 
 	struct ddb_regset *i2c;
 	struct ddb_regset *i2c_buf;
@@ -108,7 +107,6 @@ struct ddb_regmap {
 	struct ddb_regset *output;
 
 	struct ddb_regset *channel;
-	struct ddb_regset *gtl;
 };
 
 struct ddb_ids {
@@ -386,12 +384,6 @@ static inline u32 ddbreadl0(struct ddb_link *link, u32 adr)
 	return readl((char *) (link->dev->regs + (adr)));
 }
 
-static inline void gtlw(struct ddb_link *link)
-{
-	while (1 & ddbreadl0(link, link->regs + 0x10))
-		;
-}
-
 static u32 ddblreadl(struct ddb_link *link, u32 adr)
 {
 	if (unlikely(link->nr)) {
@@ -399,10 +391,8 @@ static u32 ddblreadl(struct ddb_link *link, u32 adr)
 		u32 val;
 
 		spin_lock_irqsave(&link->lock, flags);
-		gtlw(link);
 		ddbwritel0(link, adr & 0xfffc, link->regs + 0x14);
 		ddbwritel0(link, 3, link->regs + 0x10);
-		gtlw(link);
 		val = ddbreadl0(link, link->regs + 0x1c);
 		spin_unlock_irqrestore(&link->lock, flags);
 		return val;
@@ -416,7 +406,6 @@ static void ddblwritel(struct ddb_link *link, u32 val, u32 adr)
 		unsigned long flags;
 
 		spin_lock_irqsave(&link->lock, flags);
-		gtlw(link);
 		ddbwritel0(link, 0xf0000 | (adr & 0xfffc), link->regs + 0x14);
 		ddbwritel0(link, val, link->regs + 0x18);
 		ddbwritel0(link, 1, link->regs + 0x10);
@@ -434,10 +423,8 @@ static u32 ddbreadl(struct ddb *dev, u32 adr)
 		struct ddb_link *link = &dev->link[l];
 
 		spin_lock_irqsave(&link->lock, flags);
-		gtlw(link);
 		ddbwritel0(link, adr & 0xfffc, link->regs + 0x14);
 		ddbwritel0(link, 3, link->regs + 0x10);
-		gtlw(link);
 		val = ddbreadl0(link, link->regs + 0x1c);
 		spin_unlock_irqrestore(&link->lock, flags);
 		return val;
@@ -453,7 +440,6 @@ static void ddbwritel(struct ddb *dev, u32 val, u32 adr)
 		struct ddb_link *link = &dev->link[l];
 
 		spin_lock_irqsave(&link->lock, flags);
-		gtlw(link);
 		ddbwritel0(link, 0xf0000 | (adr & 0xfffc), link->regs + 0x14);
 		ddbwritel0(link, val, link->regs + 0x18);
 		ddbwritel0(link, 1, link->regs + 0x10);
@@ -463,85 +449,13 @@ static void ddbwritel(struct ddb *dev, u32 val, u32 adr)
 	writel(val, (char *) (dev->regs + (adr)));
 }
 
-static void gtlcpyto(struct ddb *dev, u32 adr, const u8 *buf,
-		     unsigned int count)
-{
-	u32 val = 0, p = adr;
-	u32 aa = p & 3;
-
-	if (aa) {
-		while (p & 3 && count) {
-			val >>= 8;
-			val |= *buf << 24;
-			p++;
-			buf++;
-			count--;
-		}
-		ddbwritel(dev, val, adr);
-	}
-	while (count >= 4) {
-		val = buf[0] | (buf[1] << 8) | (buf[2] << 16) | (buf[3] << 24);
-		ddbwritel(dev, val, p);
-		p += 4;
-		buf += 4;
-		count -= 4;
-	}
-	if (count) {
-		val = buf[0];
-		if (count > 1)
-			val |= buf[1] << 8;
-		if (count > 2)
-			val |= buf[2] << 16;
-		ddbwritel(dev, val, p);
-	}
-}
-
-static void gtlcpyfrom(struct ddb *dev, u8 *buf, u32 adr, long count)
-{
-	u32 val = 0, p = adr;
-	u32 a = p & 3;
-
-	if (a) {
-		val = ddbreadl(dev, p) >> (8 * a);
-		while (p & 3 && count) {
-			*buf = val & 0xff;
-			val >>= 8;
-			p++;
-			buf++;
-			count--;
-		}
-	}
-	while (count >= 4) {
-		val = ddbreadl(dev, p);
-		buf[0] = val & 0xff;
-		buf[1] = (val >> 8) & 0xff;
-		buf[2] = (val >> 16) & 0xff;
-		buf[3] = (val >> 24) & 0xff;
-		p += 4;
-		buf += 4;
-		count -= 4;
-	}
-	if (count) {
-		val = ddbreadl(dev, p);
-		buf[0] = val & 0xff;
-		if (count > 1)
-			buf[1] = (val >> 8) & 0xff;
-		if (count > 2)
-			buf[2] = (val >> 16) & 0xff;
-	}
-}
-
 static void ddbcpyto(struct ddb *dev, u32 adr, void *src, long count)
 {
-	if (unlikely(adr & 0xf0000000))
-		return gtlcpyto(dev, adr, src, count);
 	return memcpy_toio((char *) (dev->regs + adr), src, count);
 }
 
 static void ddbcpyfrom(struct ddb *dev, void *dst, u32 adr, long count)
 {
-	if (unlikely(adr & 0xf0000000))
-		return gtlcpyfrom(dev, dst, adr, count);
 	return memcpy_fromio(dst, (char *) (dev->regs + adr), count);
 }
 
