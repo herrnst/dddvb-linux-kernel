1c8632bd4d8d89bc421991bd15d9f97605a8afaa
Daniel Scheller <d.scheller@gmx.net>
2017-02-06 22:25:04 +0100

    [media] ddbridge: update ddbridge version

--- drivers/media/pci/ddbridge/ddbridge.h
+++ drivers/media/pci/ddbridge/ddbridge.h
@@ -555,6 +555,6 @@ static void ddbcpyfrom(struct ddb *dev, void *dst, u32 adr, long count)
 
 int ddbridge_flashread(struct ddb *dev, u32 link, u8 *buf, u32 addr, u32 len);
 
-#define DDBRIDGE_VERSION "0.9.28-integrated"
+#define DDBRIDGE_VERSION "0.9.28+v7a-integrated"
 
 #endif
