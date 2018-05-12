2b3f691997c72e659cccd9cd0ec0549a8886b4ce
Daniel Scheller <d.scheller@gmx.net>
2018-05-12 12:27:27 +0200

    [media] ddbridge: add more IOCTLs
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>

--- include/uapi/linux/ddbridge-ioctl.h
+++ include/uapi/linux/ddbridge-ioctl.h
@@ -39,6 +39,12 @@ struct ddb_flashio {
 	__u32        link;
 };
 
+/* DDB_IOCTL_GPIO_{IN,OUT} */
+struct ddb_gpio {
+	__u32 mask;
+	__u32 data;
+};
+
 /* DDB_IOCTL_ID */
 struct ddb_id {
 	/* card/PCI device data, FPGA/regmap info */
@@ -50,11 +56,67 @@ struct ddb_id {
 	__u32 regmap;
 };
 
+/* DDB_IOCTL_{READ,WRITE}_REG */
+struct ddb_reg {
+	__u32 reg;
+	__u32 val;
+};
+
+/* DDB_IOCTL_{READ,WRITE}_MEM */
+struct ddb_mem {
+	/* read/write card mem, off = offset from base address */
+	__u32        off;
+	__user __u8 *buf;
+	__u32        len;
+};
+
+/* DDB_IOCTL_{READ,WRITE}_MDIO */
+struct ddb_mdio {
+	/* read/write from/to MDIO address/register/value */
+	__u8  adr;
+	__u8  reg;
+	__u16 val;
+};
+
+/* DDB_IOCTL_{READ,WRITE}_I2C */
+struct ddb_i2c_msg {
+	/* card's I2C bus number */
+	__u8   bus;
+	/* I2C address */
+	__u8   adr;
+	/* I2C IO header */
+	__u8  *hdr;
+	__u32  hlen;
+	/* I2C message */
+	__u8  *msg;
+	__u32  mlen;
+};
+
 /* IOCTLs */
 #define DDB_IOCTL_FLASHIO \
 	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x00), struct ddb_flashio)
+#define DDB_IOCTL_GPIO_IN \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x01), struct ddb_gpio)
+#define DDB_IOCTL_GPIO_OUT \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x02), struct ddb_gpio)
 #define DDB_IOCTL_ID \
 	_IOR(DDB_IOCTL_MAGIC,  (DDB_IOCTL_SEQIDX + 0x03), struct ddb_id)
+#define DDB_IOCTL_READ_REG \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x04), struct ddb_reg)
+#define DDB_IOCTL_WRITE_REG \
+	_IOW(DDB_IOCTL_MAGIC,  (DDB_IOCTL_SEQIDX + 0x05), struct ddb_reg)
+#define DDB_IOCTL_READ_MEM \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x06), struct ddb_mem)
+#define DDB_IOCTL_WRITE_MEM \
+	_IOR(DDB_IOCTL_MAGIC,  (DDB_IOCTL_SEQIDX + 0x07), struct ddb_mem)
+#define DDB_IOCTL_READ_MDIO \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x08), struct ddb_mdio)
+#define DDB_IOCTL_WRITE_MDIO \
+	_IOR(DDB_IOCTL_MAGIC,  (DDB_IOCTL_SEQIDX + 0x09), struct ddb_mdio)
+#define DDB_IOCTL_READ_I2C \
+	_IOWR(DDB_IOCTL_MAGIC, (DDB_IOCTL_SEQIDX + 0x0A), struct ddb_i2c_msg)
+#define DDB_IOCTL_WRITE_I2C \
+	_IOR(DDB_IOCTL_MAGIC,  (DDB_IOCTL_SEQIDX + 0x0B), struct ddb_i2c_msg)
 
 /******************************************************************************/
 
