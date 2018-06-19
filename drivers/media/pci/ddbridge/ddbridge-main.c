3c5ef9efb25ff501865c5b3eecf03256e19359ed
Daniel Scheller <d.scheller@gmx.net>
2018-06-19 19:03:04 +0200

    [media] ddbridge: header/boilerplate cleanups and cosmetics
    Cleanup whitespaces and blank lines, remove wrong links to
    http://www.gnu.org/copyleft/gpl.html (the driver is licensed under the
    terms of GPLv2, but the link points to a copy of the GPLv3), and fix
    the filename reference in ddbridge-i2c.h.
    
    Cc: Ralph Metzler <rjkm@metzlerbros.de>
    Cc: Manfred Voelkel <mvoelkel@DigitalDevices.de>
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>

--- drivers/media/pci/ddbridge/ddbridge-main.c
+++ drivers/media/pci/ddbridge/ddbridge-main.c
@@ -12,9 +12,8 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
